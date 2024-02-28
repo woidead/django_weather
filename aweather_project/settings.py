168b03be18e18f0b2b27b6d422335a554b40b567
woidead <abikbest05@gmail.com>
2024-02-28 16:55:01 +0600

    added .env params

--- aweather_project/settings.py
+++ aweather_project/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dxet!qnun3nk9!%p7f+pq7-c+tat3fq-s!e8^j#!7etlkepp*k')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dxet!qnun3nk9!%p7f+pq7-c+tat3fq-s!e8^j#!7etlkepp*k'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+API_KEY = os.getenv('API_KEY', None)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weather_app',
 ]
 
 MIDDLEWARE = [

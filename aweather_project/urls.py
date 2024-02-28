d7d87155503b07520ca4287510e2793953cdabb5
woidead <abikbest05@gmail.com>
2024-02-28 17:45:38 +0600

    version 1.0

--- aweather_project/urls.py
+++ aweather_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('weather_app.urls'))
+    path('', include('weather_app.urls')),
 ]
